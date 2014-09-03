14a33423924d5b863ecdf60c38bbc2f492f88dd9
Jesse Gardner <jesse@plasticmind.com>
2014-09-03 16:27:11 -0400

    Fixed bad favicon reference

--- header.php
+++ header.php
@@ -10,7 +10,7 @@
   <meta charset="<?php bloginfo( 'charset' ); ?>" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=yes" />
   <title><?php wp_title( '//', true, 'right' ); ?></title>
-  <link rel="shortcut icon" href="favicon.ico?v=3" />
+  <link rel="shortcut icon" href="/favicon.ico?v=3" />
   <!--[if lt IE 9]>
   <script src="<?php echo get_template_directory_uri(); ?>/assets/js/vendor/html5shiv.js" type="text/javascript"></script>
   <![endif]-->
