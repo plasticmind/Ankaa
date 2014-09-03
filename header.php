1c3cb044475d3987cae2afb6807629f55de42520
Jesse Gardner <jesse@plasticmind.com>
2014-09-03 11:05:23 -0400

    Fixed broken HTML5 shiv for IE

--- header.php
+++ header.php
@@ -12,7 +12,7 @@
   <title><?php wp_title( '//', true, 'right' ); ?></title>
   <link rel="profile" href="http://gmpg.org/xfn/11" />
   <!--[if lt IE 9]>
-  <script src="<?php echo get_template_directory_uri(); ?>/js/html5.js" type="text/javascript"></script>
+  <script src="<?php echo get_template_directory_uri(); ?>/assets/js/vendor/html5shiv.js" type="text/javascript"></script>
   <![endif]-->
   <?php wp_head(); ?>
 </head>
