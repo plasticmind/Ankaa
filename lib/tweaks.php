7bfaa97ee1986a64c0f6445282b1f1854ec30c11
Jesse Gardner <jesse@plasticmind.com>
2017-03-17 08:21:05 -0400

    Pinterest verification

--- lib/tweaks.php
+++ lib/tweaks.php
@@ -4,10 +4,10 @@
  */
 
 // == Replace default separator for HTML title
-add_filter('wpseo_replacements_filter_sep','pm_replace_separator');
 function pm_replace_separator() {
 	return '//';
 }
+add_filter('wpseo_replacements_filter_sep','pm_replace_separator');
 
 // == Replace default excerpt link
 
@@ -20,3 +20,11 @@ function new_excerpt_more( $more ) {
 	return '... <a class="read-more" href="'. get_permalink( get_the_ID() ) . '">' . 'Read More' . '</a>';
 }
 add_filter( 'excerpt_more', 'new_excerpt_more' );
+
+// == Add Pinterest verification to site header
+
+function pm_pinterest_verify() {
+	$output='<meta name="p:domain_verify" content="913d207e750e1f7931023b1b45fecc07"/>';
+	echo $output;
+}
+add_action('wp_head','pm_pinterest_verify');
