1d500bd94abd9134094fab0e430d540d5dd8e984
Jesse Gardner <jesse@plasticmind.com>
2019-03-14 09:24:50 -0400

    Add Mailchimp editable areas

--- single-newsletter.php
+++ single-newsletter.php
@@ -547,6 +547,41 @@
                     <tr>
                       <td valign="top" class="bodyContainer">
 
+  <!--ENTRY BODY-->
+
+  <table border="0" cellpadding="0" cellspacing="0" width="100%" class="mcnTextBlock" style="min-width:100%;" >
+    <tbody class="mcnTextBlockOuter">
+      <tr>
+        <td valign="top" class="mcnTextBlockInner" style="padding-top:9px;">
+          <!--[if mso]>
+          <table align="left" border="0" cellspacing="0" cellpadding="0" width="100%" style="width:100%;">
+          <tr>
+          <![endif]-->
+          
+          <!--[if mso]>
+          <td valign="top" width="600" style="width:600px;">
+          <![endif]-->
+          <table align="left" border="0" cellpadding="0" cellspacing="0" style="max-width:100%; min-width:100%;" width="100%" class="mcnTextContentContainer" mc:edit="body">
+            <tbody><tr>
+              <td valign="top" class="mcnTextContent" style="padding-top:0; padding-right:18px; padding-bottom:9px; padding-left:18px;">
+              
+                <p>This part of the template should be editable in Mailchimp.</p>
+
+              </td>
+            </tr>
+          </tbody></table>
+          <!--[if mso]>
+          </td>
+          <![endif]-->
+          
+          <!--[if mso]>
+          </tr>
+          </table>
+          <![endif]-->
+        </td>
+      </tr>
+    </tbody>
+  </table>
 
 <!--ENTRY LOOP-->
 
