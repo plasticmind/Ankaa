e5020fcdff0867f38662a6552b31d301c3bc2015
Jesse Gardner <jesse@plasticmind.com>
2019-12-11 16:05:12 -0500

    Update footer

--- footer.php
+++ footer.php
@@ -1,7 +1,7 @@
 			<footer class="site-footer">
 				<div class="copyright">
 					<p>Copyright &copy; 1999-<?php echo date('Y'); ?> Jesse Gardner</p>
-					<p class="thanks">Thank you for reading my site.</p>
+					<p class="thanks">Thanks for stopping by.</p>
 				</div>
 			</footer>
 		</div>
